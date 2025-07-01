7c7ca37fdd62dba129eb2fd1a2fe56882b39aae0
hussainahmad <hussainahmad.dev.17@gmail.com>
2025-07-01 23:58:39 +0500

    project completed

---
+++ middlewares/authentication.js
@@ -0,0 +1,30 @@
+const { validateToken } = require("../services/authentication");
+
+function checkForAuthenticationCookie(cookieName) {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      return next();
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
+
+    return next();
+  };
+}
+
+
+const checkAuth = async (req,res, next) => {
+  if(!req.user){
+     res.render("signin")
+  }
+ return next();
+}
+
+module.exports = {
+  checkForAuthenticationCookie,
+  checkAuth
+};
